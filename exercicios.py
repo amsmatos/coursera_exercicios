dbede07ebbde78b15134c166b08be093e89ea8bf
AM <109461442+amsmatos@users.noreply.github.com>
2023-01-29 15:59:53 +0000

    1º envio

---
+++ exercicios.py
@@ -0,0 +1,27 @@
+# Try out the enumerate function for yourself in this quick exercise. 
+# Complete the skip_elements function to return every other element from the list, 
+# this time using the enumerate function to check if an element is in an even position or an odd position.
+
+''' Exercício :
+def skip_elements(elements):
+	# code goes here
+	
+	return ___
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+
+Minha resposta abaixo'''
+
+def skip_elements(elements):
+    # code goes here
+    elements2 = []
+    i = 0
+    for element in elements:
+        if i % 2 == 0:
+            elements2.append(element)
+        i += 1
+    return elements2
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
