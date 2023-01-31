b7899b7bbf495f86badbd820c8d411b0f7643291
AM <109461442+amsmatos@users.noreply.github.com>
2023-01-31 23:39:23 +0000

    31.01.2023

--- exercicios.py
+++ exercicios.py
@@ -1062,3 +1062,117 @@ class Person:
 some_person = Person("Roger")  
 # Call the greeting method
 print(some_person.greeting())
+
+
+'''
+# The code below defines an Elevator class. The elevator has a current floor, it also has a top and a bottom floor that are the minimum and maximum floors it can go to. 
+# Fill in the blanks to make the elevator go through the floors requested.
+
+class Elevator:
+    def __init__(self, bottom, top, current):
+        """Initializes the Elevator instance."""
+        pass
+    def up(self):
+        """Makes the elevator go up one floor."""
+        pass
+    def down(self):
+        """Makes the elevator go down one floor."""
+        pass
+    def go_to(self, floor):
+        """Makes the elevator go to the specific floor."""
+        pass
+
+elevator = Elevator(-1, 10, 0)
+
+elevator.up() 
+elevator.current #should output 1
+
+elevator.down() 
+elevator.current #should output 0
+
+elevator.go_to(10) 
+elevator.current #should output 10
+
+# Go to the top floor. Try to go up, it should stay. Then go down.
+elevator.go_to(10)
+elevator.up()
+elevator.down()
+print(elevator.current) # should be 9
+# Go to the bottom floor. Try to go down, it should stay. Then go up.
+elevator.go_to(-1)
+elevator.down()
+elevator.down()
+elevator.up()
+elevator.up()
+print(elevator.current) # should be 1
+
+# Now add the str method to your Elevator class definition above so that when printing the elevator using the print( ) method, 
+# we get the current floor together with a message. For example, in the 5th floor it should say "Current floor: 5"
+
+elevator.go_to(5)
+print(elevator)
+'''
+
+# Minha resposta
+
+class Elevator:
+    def __init__(self, bottom, top, current):
+        """Initializes the Elevator instance."""
+        self.bottom = bottom
+        self.top = top
+        self.current = current
+    def __str__(self):
+        return "Current floor: {}".format(self.current)
+    def up(self):
+        """Makes the elevator go up one floor."""
+        if self.current <= self.top and self.current >= self.bottom:
+            if self.current != self.top:          
+                self.current += 1
+                return self.current
+    def down(self):
+        """Makes the elevator go down one floor."""
+        if self.current <= self.top and self.current >= self.bottom:
+            if self.current != self.bottom:
+                self.current -= 1
+                return self.current
+    def go_to(self, floor):
+        """Makes the elevator go to the specific floor."""
+        if self.current < self.top and self.current > self.bottom:
+            self.current = floor
+            return self.current
+        
+
+elevator = Elevator(-1, 10, 0)
+
+'''
+
+# Let’s create a new class together and inherit from it. Below we have a base class called Clothing.
+# Together, let’s create a second class, called Shirt, that inherits methods from the Clothing class. 
+# Fill in the blanks to make it work properly.
+
+class Clothing:
+  material = ""
+  def __init__(self,name):
+    self.name = name
+  def checkmaterial(self):
+	  print("This {} is made of {}".format(self.___,self.___))
+			
+class Shirt(___):
+  material="Cotton"
+
+polo = Shirt("Polo")
+polo.checkmaterial()
+
+'''
+
+# Minha resposta
+
+class Clothing:
+  material = ""
+  def __init__(self,name):
+    self.name = name
+  def checkmaterial(self):
+	  print("This {} is made of {}".format(self.name,self.material))
+			
+class Shirt(Clothing):
+  material="Cotton"
